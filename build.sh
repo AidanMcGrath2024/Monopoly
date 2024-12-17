a276e9cff41a0b3224d22c6994c19f03b013139a
AidanMcGrath2024 <awm6052@psu.edu>
2024-12-17 11:29:26 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,30 @@ project_root_dir=$current_directory
 
 echo "Hello Friend! Let's try and build this application together :D"
 
-
 # compile the rest of the application
 cd ${project_root_dir}
 
+mkdir -p ${project_root_dir}/third_party
+cd ${project_root_dir}/third_party
+if [ ! -d "sfml_lib" ]; then
+    git clone https://github.com/SFML/SFML.git sfml_lib
+    cd ${project_root_dir}/third_party/sfml_lib
+    git fetch --tags --depth 1
+    git checkout -b 2.6.2 tags/2.6.2
+    
+    rm -rf build install
+    mkdir -p build && mkdir -p install && cd build
+    cmake -DCMAKE_INSTALL_PREFIX="${project_root_dir}/third_party/sfml_lib/install" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS:BOOL=ON ..
+    cmake --build . --config Release -j 4
+    cmake --install . --config Release
+
+    mkdir -p "${project_root_dir}/third_party/sfml_lib/install/Frameworks/freetype.framework"
+    cp -r "${project_root_dir}/third_party/sfml_lib/install/lib/freetype.framework" "${project_root_dir}/third_party/sfml_lib/install/Frameworks/"
+fi
+
 # remove the build directory that has the current code in it
 echo "deleting the BUILD directory"
-rm -rf ${project_root_dir}/build
+# rm -rf ${project_root_dir}/build
 rm -rf ${project_root_dir}/install
 
 echo "make a new BUILD directory to start the compiling process"
@@ -30,8 +47,6 @@ cp -r ${project_root_dir}/Images ${project_root_dir}/build/Images
 cd ${project_root_dir}
 echo "declare success -- hooray!"
 
-echo "running the executable with some default parameters"
-echo "./build/main -c config.inp > results.txt 2>&1"
-echo "  the 2>&1 redirects the stderr to a 1 so we don't see the gnuplot problems"
-mkdir -p ${project_root_dir}/results
-./build/main -c config.inp > results/results.txt 2>&1
+echo "running the executable ..."
+cd ${project_root_dir}/build
+./main
